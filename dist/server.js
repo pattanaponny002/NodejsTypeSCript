2b55d08a5f83038aece51c3d5f734750b8507b81
unknown <oloponnyolo@gmail.com>
2023-08-29 13:03:41 +0700

    add profile

--- dist/server.js
+++ dist/server.js
@@ -11,4 +11,4 @@ app.use("/api", user_route_1.default);
 app.get("/", (req, res, next) => {
     res.json({ message: "messaging" });
 });
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server is running on port 5000"));
--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,6 @@ app.use("/api", UserRouter);
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ message: "messaging" });
 });
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server is running on port 5000")
+);
